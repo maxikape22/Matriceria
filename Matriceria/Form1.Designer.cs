0a4c4b30fc502d7a900eaeb076ccaa2f809d3b87
javier barrera <fcbjavier12@hotmail.com>
2024-06-07 00:49:03 -0300

    cambios

--- Matriceria/Form1.Designer.cs
+++ Matriceria/Form1.Designer.cs
@@ -66,7 +66,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(433, 146);
+            this.button3.Location = new System.Drawing.Point(434, 146);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
